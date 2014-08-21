31723fd9320396179c3b198c3b595faae58cf76f
Benoit Landry <landry@mit.edu>
2014-08-21 14:54:09 -0700

    Update copyright date

--- qspline.c
+++ qspline.c
@@ -1,5 +1,5 @@
 /*
-COPYRIGHT (C) 2014 by James McEnnan, Planet Labs
+COPYRIGHT (C) 2003 by James McEnnan, 2014 by Planet Labs
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by

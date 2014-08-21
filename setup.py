73f4cee0a127ea2a0bf0d3b3ecc7ebd048cf5676
Benoit Landry <landry@mit.edu>
2014-08-20 17:21:06 -0700

    updating author

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 setup(name = 'pyqspline',
       version = '0.1',
-      author = 'Benoit Landry',
+      author = 'James McEnnan, Planet Labs',
       description = 'Python version of qspline, which produces a quaternion spline interpolation of sparse data.',
       py_modules = ['pyqspline'],
       ext_modules = [Extension('qspline', sources=['qspline.c'])])

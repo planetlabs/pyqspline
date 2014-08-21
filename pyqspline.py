3f2b283f9c159a551e6c36098236a1893ee5b2f8
Benoit Landry <landry@mit.edu>
2014-08-20 17:19:26 -0700

    adding some docstrings

--- pyqspline.py
+++ pyqspline.py
@@ -1,6 +1,25 @@
 import qspline
 
 def interpolate(n,ns,ds,maxit,tol,wi,wf,x,y):
+    """ Interpolate quaternions
+
+    Args:
+        n: number of input points (n >= 4).
+        ns: number of output points (ns >= 2).
+        ds: interval between output points (if ds>0.0, it is used over ns).
+        maxit: maximum number of iterations.
+        tol: convergence tolerance (rad/sec) for iteration termination.
+        wi: initial angular rate vector [wx,wy,wz].
+        wf: final angular rate vector [wx,wy,wz].
+        x: input list of time values.
+        y: input list of quaternion lists [qx,qy,qz,qs].
+    Returns:
+        t: output list of time values.
+        q: output list of interpolated quaternion values.
+        omega: output list of interpolated angular rate [wx,wy,wz] (rad/sec).
+        alpha: output list of interpolated angular acceleration [ax,ay,az] (rad/sec^2).
+
+    """
 
     if ds>0:
         ns = int((x[-1]-x[0])/ds)+1
@@ -33,13 +52,34 @@ def interpolate(n,ns,ds,maxit,tol,wi,wf,x,y):
     return [t,unflatten(q,ns,4),unflatten(omega,ns,3),unflatten(alpha,ns,3)]
 
 def flatten(nestedlist):
+    """ Flattens a list
+
+    Args:
+        nestedlist: a list of lists
+    Returns:
+        a list containing all the elements of nestedlist
+
+    """
+
     return [item for sub in nestedlist for item in sub] 
 
 def unflatten(flatlist,m,n):
+    """ Construct a nested list from a flat list
+
+    Args:
+        flatlist: a list of elements
+        m: the number of lists inside the final list
+        n: the number of element in each sublist
+    Returns:
+        a nested list of size m, each with n elements
+
+    """
+
     out = []
     for r in range(m):
         row = []
         for c in range(n):
             row.append(flatlist[n*r+c])
         out.append(row)
+        
     return out

083b470d786a41fa5abca1deca4767b2e43be7f1
Erika Reinhardt <embreinhardt@gmail.com>
2014-08-27 11:21:19 -0700

    Lint python files

--- pyqspline.py
+++ pyqspline.py
@@ -11,20 +11,23 @@ COPYRIGHT (C) 2014 by Planet Labs
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA.
 """
 
 import qspline
 
-def interpolate(n,ns,ds,maxit,tol,wi,wf,x,y):
+
+def interpolate(n, ns, ds, maxit, tol, wi, wf, x, y):
     """ Interpolate quaternions
 
     Args:
         n: number of input points (n >= 4).
         ns: number of output points (ns >= 2).
-        ds: interval between output points (if ds>0.0, it is used over ns).
+        ds: interval between output points
+            (if ds>0.0, it is used over ns).
         maxit: maximum number of iterations.
         tol: convergence tolerance (rad/sec) for iteration termination.
         wi: initial angular rate vector [wx,wy,wz].
@@ -34,40 +37,49 @@ def interpolate(n,ns,ds,maxit,tol,wi,wf,x,y):
     Returns:
         t: output list of time values.
         q: output list of interpolated quaternion values.
-        omega: output list of interpolated angular rate [wx,wy,wz] (rad/sec).
-        alpha: output list of interpolated angular acceleration [ax,ay,az] (rad/sec^2).
+        omega: output list of interpolated angular rate [wx,wy,wz]
+            (rad/sec).
+        alpha: output list of interpolated angular acceleration
+            [ax,ay,az] (rad/sec^2).
 
     """
 
-    if ds>0:
-        ns = int((x[-1]-x[0])/ds)+1
+    if ds > 0:
+        ns = int((x[-1] - x[0]) / ds) + 1
 
     # tuples throw off qspline, check sizes and also
     # make sure they are list objects
-    if n<4:
+    if n < 4:
         raise ValueError('n must be greater or equal to 4')
-    if ns<2:
+    if ns < 2:
         raise ValueError('ns must be greater or equal to 2')
     wi = list(wi)
-    if len(wi)!=3:
+    if len(wi) != 3:
         raise ValueError('wi must be size 3')
     wf = list(wf)
-    if len(wf)!=3:
+    if len(wf) != 3:
         raise ValueError('wf must be size 3')
     x = list(x)
-    if len(x)!=n:
+    if len(x) != n:
         raise ValueError('x must have the same length as n')
     y = list(y)
-    if len(y)!=n:
+    if len(y) != n:
         raise ValueError('y must have the same length as y')
     for i in range(n):
         y[i] = list(y[i])
-        if len(y[i])!=4:
+        if len(y[i]) != 4:
             raise ValueError('y must have 4 columns')
 
-    t,q,omega,alpha = qspline.pytocqspline(n,ns,ds,maxit,tol,wi,wf,x,flatten(y))
-    
-    return [t,unflatten(q,ns,4),unflatten(omega,ns,3),unflatten(alpha,ns,3)]
+    t, q, omega, alpha = qspline.pytocqspline(
+        n, ns, ds, maxit, tol, wi, wf, x, flatten(y))
+
+    return [
+        t,
+        unflatten(q, ns, 4),
+        unflatten(omega, ns, 3),
+        unflatten(alpha, ns, 3),
+    ]
+
 
 def flatten(nestedlist):
     """ Flattens a list
@@ -79,9 +91,10 @@ def flatten(nestedlist):
 
     """
 
-    return [item for sub in nestedlist for item in sub] 
+    return [item for sub in nestedlist for item in sub]
 
-def unflatten(flatlist,m,n):
+
+def unflatten(flatlist, m, n):
     """ Construct a nested list from a flat list
 
     Args:
@@ -97,7 +110,7 @@ def unflatten(flatlist,m,n):
     for r in range(m):
         row = []
         for c in range(n):
-            row.append(flatlist[n*r+c])
+            row.append(flatlist[n * r + c])
         out.append(row)
-        
+
     return out
--- tests/test.py
+++ tests/test.py
@@ -11,9 +11,10 @@ COPYRIGHT (C) 2014 by Planet Labs
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301 USA.
 """
 
 import os
@@ -21,38 +22,48 @@ import math
 import unittest
 import pyqspline
 
+
 class PyQsplineTest(unittest.TestCase):
 
     def test(self):
 
-        samplespath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'samples.dat')
-        nominalpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'nominal.dat')
+        samplespath = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'samples.dat')
+        nominalpath = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), 'nominal.dat')
 
-        datafile = open(samplespath,'r')
+        datafile = open(samplespath, 'r')
         wi = [float(w) for w in datafile.readline().split(' ')]
         wf = [float(w) for w in datafile.readline().split(' ')]
-        nout = int(datafile.readline())
+
         x = []
         y = []
         for row in datafile.readlines():
-            t,qx,qy,qz,qs = [float(q) for q in row.strip().split(' ') if q!='']
+            t, qx, qy, qz, qs = [
+                float(q) for q in row.strip().split(' ') if q != '']
             x.append(t)
-            y.append([qx,qy,qz,qs])
+            y.append([qx, qy, qz, qs])
 
-        rt,rq,romega,ralpha = pyqspline.interpolate(len(x),0,0.5,10,0.000001,wi,wf,x,y)
+        rt, rq, romega, ralpha = pyqspline.interpolate(
+            len(x), 0, 0.5, 10, 0.000001, wi, wf, x, y)
 
-        ndatafile = open(nominalpath,'r')
-        rows =  ndatafile.readlines()
+        ndatafile = open(nominalpath, 'r')
+        rows = ndatafile.readlines()
         nt = []
         nq = []
         nomega = []
         nalpha = []
-        for i in range(len(rows)/3):
-            t,qx,qy,qz,qs = [x for x in rows[i*3].strip().split(' ') if x!='']
+        for i in range(len(rows) / 3):
+            t, qx, qy, qz, qs = [
+                x for x in rows[i * 3].strip().split(' ') if x != '']
             nt.append(float(t))
-            nq.append([float(qx),float(qy),float(qz),float(qs)])
-            nomega.append([float(x) for x in rows[i*3+1].strip().split(' ') if x!=''])
-            nalpha.append([float(x) for x in rows[i*3+2].strip().split(' ') if x!=''])
+            nq.append([float(qx), float(qy), float(qz), float(qs)])
+            nomega.append([
+                float(x)
+                for x in rows[i * 3 + 1].strip().split(' ') if x != ''])
+            nalpha.append([
+                float(x)
+                for x in rows[i * 3 + 2].strip().split(' ') if x != ''])
 
         testcount = 0
         tol = 1e-5
@@ -67,10 +78,24 @@ class PyQsplineTest(unittest.TestCase):
                 rqstar = rq[ri]
                 romegastar = romega[ri]
                 ralphastar = ralpha[ri]
-                self.assertLess(math.sqrt(sum([(qa-qb)**2 for qa, qb in zip(rqstar,nqstar)])),tol)
-                self.assertLess(math.sqrt(sum([(oa-ob)**2 for oa, ob in zip(romegastar,nomegastar)])),tol)
-                self.assertLess(math.sqrt(sum([(aa-ab)**2 for aa, ab in zip(ralphastar,nalphastar)])),tol)
-        self.assertGreater(testcount,0,'Could not find the initial sample points in the resulting interpolation. No testing possible.')
+                self.assertLess(
+                    math.sqrt(sum(
+                        [(qa - qb) ** 2 for qa, qb in zip(rqstar, nqstar)])),
+                    tol)
+                self.assertLess(
+                    math.sqrt(sum([
+                        (oa - ob) ** 2
+                        for oa, ob in zip(romegastar, nomegastar)])),
+                    tol)
+                self.assertLess(
+                    math.sqrt(sum([
+                        (aa - ab) ** 2
+                        for aa, ab in zip(ralphastar, nalphastar)])),
+                    tol)
+        self.assertGreater(
+            testcount, 0,
+            ('Could not find the initial sample points in the resulting '
+             'interpolation. No testing possible.'))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()

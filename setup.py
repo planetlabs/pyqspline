5f03f1bf32abe35d4fea476a564f2ccbac356a9b
Michael Weisman <michael.weisman@planet.com>
2021-01-15 16:24:32 -0800

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup, Extension
 
 setup(name = 'pyqspline',
-      version = '0.1',
+      version = '0.2',
       author = 'James McEnnan, Planet Labs',
       description = 'Python version of qspline, which produces a quaternion spline interpolation of sparse data.',
       py_modules = ['pyqspline'],
